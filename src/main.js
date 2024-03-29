21d2f486ddca2208f6242d2b6f19342810858d37
BlackLion1313 <mila.alenina@gmail.com>
2024-03-29 14:57:22 +0100

    filters

--- src/main.js
+++ src/main.js
@@ -1,138 +1,108 @@
-
-const apiUrl = 'https://randomuser.me/api/?results=10';
+const apiUrl = 'https://randomuser.me/api/?results=25';
 
 async function fetchUsers() {
   try {
     const response = await fetch(apiUrl);
     const result = await response.json();
-    return result.results
-
+    return result.results;
   } catch (error) {
     console.log('error', error);
   }
 }
 
-
-
 async function functionsController() {
   try {
     const users = await fetchUsers();
-
     createListWithUsers(users);
     createDropDown(users);
     setEventListeners(users);
+    filterByDropDown(users);
   } catch (error) {
-    console.log('error', error)
+    console.log('error', error);
   }
-
 }
-
 functionsController();
 
-function createListWithUsers(users) {
-  const tbody = document.getElementById('tbody')
+const createListWithUsers = (users) => {
+  const tbody = document.getElementById('tbody');
   tbody.innerHTML = '';
 
-  for (let i = 0; i < users.length; i++) {
-    const row = document.createElement('tr');
-    row.addEventListener('click', () => {
-      openModal(users[i])
-    })
-    const tableH = document.createElement('th');
-    tableH.scope = 'row';
-    tableH.innerText = i + 1
-    const tdFirstName = document.createElement('td');
-    const tdLastName = document.createElement('td');
-    const tdLocation = document.createElement('td');
-    tdFirstName.innerText = users[i].name.first
-    tdLastName.innerText = users[i].name.last
-    tdLocation.innerText = users[i].location.city
+  users.forEach((user, i) => {
+    if (isInDropdown(user) && isInput(user) && isGenderRadio(user)) {
+      const row = document.createElement('tr');
+      row.addEventListener('click', () => {
+        openModal(users[i]);
+      });
+      const tableH = document.createElement('th');
+      tableH.scope = 'row';
+      tableH.innerText = i + 1;
+      const tdFirstName = createTableCell(user.name.first);
+      const tdLastName = createTableCell(user.name.last);
+      const tdAge = createTableCell(user.dob.age);
+      const tdEmail = createTableCell(user.email);
+      const tdPhone = createTableCell(user.cell);
+      const tdCity = createTableCell(user.location.city);
+      const tdState = createTableCell(user.location.state);
+      const tdCountry = createTableCell(user.location.country);
 
-    tbody.appendChild(row)
-    row.appendChild(tableH)
-    row.appendChild(tdFirstName)
-    row.appendChild(tdLastName)
-    row.appendChild(tdLocation)
+      appendCellsToRow(row, [tableH, tdFirstName, tdLastName, tdAge, tdEmail, tdPhone, tdCity, tdState, tdCountry]);
+      tbody.appendChild(row);
+    }
+  });
+};
 
+const createTableCell = (value) => {
+  const td = document.createElement('td');
+  td.innerText = value;
+  return td;
+};
 
-  }
-
-}
+const appendCellsToRow = (row, cells) => {
+  cells.forEach(cell => {
+    row.appendChild(cell);
+  });
+};
 
 function createDropDown(users) {
-  const genderSelect = document.getElementById('gender');
-  const countrySelect = document.getElementById('country');
+  const citySelect = document.getElementById('city');
+  const cities = users.map(user => user.location.city);
+  const uniqueCity = [...new Set(cities)];
 
-  const genders = users.map((user) => {
-    return user.gender
-  })
-
-  const countries = users.map((user) => {
-
-    return user.location.country
-  })
-  console.log('countries', countries)
-
-  const uniqueGender = [...new Set(genders)];
-  console.log('uniqueGender', uniqueGender)
-
-  const uniqueCountry = [... new Set(countries)]
-  console.log('uniqueCountry', uniqueCountry)
-
-  uniqueGender.forEach((gender) => {
-    const optionGender = document.createElement('option');
-    optionGender.innerText = gender;
-    optionGender.value = gender;
-    genderSelect.appendChild(optionGender);
-  })
-
-  uniqueCountry.forEach((country) => {
-    const optionCountry = document.createElement('option');
-    optionCountry.innerText = country;
-    optionCountry.value = country;
-    countrySelect.appendChild(optionCountry);
-
-  })
-
+  uniqueCity.forEach(city => {
+    const optionCity = document.createElement('option');
+    optionCity.innerText = city;
+    optionCity.value = city;
+    citySelect.appendChild(optionCity);
+  });
 }
-
 
 const setEventListeners = (users) => {
-  document.querySelector('select').addEventListener('change', (event) => {
-    filterByDropDown(users)
-  })
-}
-
-function filterByDropDown(users) {
-  const selectedOptionGender = document.getElementById('gender').value;
-  if (selectedOptionGender === 'all') {
-    createListWithUsers(users);
-  } else {
-    const filteredGender = users.filter((user) => {
-      return user.gender === selectedOptionGender;
+  const selects = document.querySelectorAll('select');
+  selects.forEach(select => {
+    select.addEventListener('change', () => {
+      createListWithUsers(users);
     });
+  });
 
-    createListWithUsers(filteredGender);
-  }
-
-  const selectedOptionCountry = document.getElementById('country').value;
-  if (selectedOptionCountry === 'all') {
+  const searchInput = document.getElementById('searchInput');
+  searchInput.addEventListener('input', () => {
     createListWithUsers(users);
-  } else {
-    const filteredCountry = users.filter((user) => {
-      return user.location.country === selectedOptionCountry;
-    });
+  });
 
-    createListWithUsers(filteredCountry);
-  }
-}
+  const genderRadios = document.querySelectorAll('input[type="radio"][name="genderFilter"]');
+  genderRadios.forEach(radio => {
+    radio.addEventListener('change', () => {
+      createListWithUsers(users);
+    });
+  });
+};
+
 
 function openModal(user) {
-  const modalContent = document.getElementById('modal-body')
-
+  const modalContent = document.getElementById('modal-body');
   modalContent.innerHTML = `
-    <h2 class= 'text-center'> ${user.name.first} ${user.name.last}</h2>
-    <img class = 'img-fluid'  src = "${user.picture.large}" alt="user picture"></img>
+    <h2 class='text-center'> ${user.name.first} ${user.name.last}</h2>
+    <img class='img-fluid' src="${user.picture.large}" alt="user picture"></img>
   `;
   const modal = document.getElementById('myModal');
   modal.style.display = 'block';
@@ -140,21 +110,29 @@ function openModal(user) {
   const closeModal = document.getElementById('close');
   closeModal.addEventListener('click', () => {
     modal.style.display = 'none';
-
-  })
-
-
+  });
 }
 
-
 function writeEmail() {
-  document.getElementById('email').addEventListener('click', () => {
+  const emailLink = document.getElementById('email');
+  emailLink.addEventListener('click', () => {
     window.location.href = "mailto:recipient@example.com";
-  })
-
+  });
 }
 writeEmail();
 
+const isInDropdown = (user) => {
+  const selectedOptionCity = document.getElementById('city').value;
+  return selectedOptionCity === user.location.city || selectedOptionCity === 'all';
+};
 
+const isInput = (user) => {
+  const searchInputValue = document.getElementById('searchInput').value.toLowerCase();
+  const fullName = `${user.name.first} ${user.name.last}`.toLowerCase();
+  return fullName.includes(searchInputValue);
+};
 
-
+const isGenderRadio = (user) => {
+  const genderRadios = document.querySelector('input[type="radio"][name="genderFilter"]:checked').value;
+  return genderRadios === 'all' || user.gender === genderRadios;
+};
